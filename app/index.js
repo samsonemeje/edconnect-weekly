10de95b0d161883d7bdace56212c9f92da2fc476
samsonemeje <samsonemeje@gmail.com>
2021-02-18 23:46:01 +0100

    Created HTML pages for Project Explorer

--- app/index.js
+++ app/index.js
@@ -6,4 +6,6 @@ const SERVER_PORT = 4000;
 
 app.use(express.static('public'));
 
-app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
+app.listen(SERVER_PORT, () =>
+	console.log('Server listening on port ' + SERVER_PORT)
+);
